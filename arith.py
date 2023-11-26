a8d3c378487bfe778b1ddaf7828f38ebef843402
charan <charans5660@gmail.com>
2023-11-26 16:28:20 +0530

    substraction function will be added

--- arith.py
+++ arith.py
@@ -2,7 +2,7 @@ num1 = int(input("Enter First Number: "))
 num2 = int(input("Enter Second Number: "))
 
 print("Enter which operation would you like to perform?")
-ch = input("Enter any of these char for specific operation +,-,*,/: ")
+ch = input("Enter any of these char for specific operation +,-,*,/,%: ")
 
 result = 0
 if ch == '+':
@@ -13,5 +13,7 @@ elif ch == '*':
     result = num1 * num2
 elif ch == '/':
     result = num1 / num2
+elif ch== '%':
+    result = num1 % num2
 else:
     print("Input character is not recognized!")
